5f9ba5fd8ef22cab54511e018b50c234c310c884
Sarp Küçük <sarpkucuk@gmail.com>
2025-02-04 16:48:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
-sudo pacman -S wireguird thunderbird bauh ns-usbloader redshift rofi bottom simple-usb-automount-git fastfetch foot flatpak qbittorrent megasync && flatpak install zen-browser telegram ente-auth
+sudo pacman -S wireguard
+sudo pacman -S thunderbird
+sudo pacman -S bauh
+sudo pacman -S ns-usbloader 
+sudo pacman -S redshift
+sudo pacman -S gtop
+sudo pacman -S ulauncher
+sudo pacman -S simple-usb-automount-git sudo pacman -S fastfetch
+sudo pacman -S foot
+sudo pacman -S wezterm
+sudo pacman -S qbittorrent
+sudo pacman -S megasync
+sudo pacman -S flatpak
+flatpak install zen-browser
+flatpak install telegram
